8f38fd016bff08a03d9156e72a69535644c33ef3
unknown <lawrencegeronimo27@gmail.com>
2025-09-17 12:11:58 +0800

    Update project files

--- app/models/StudentsModel.php
+++ app/models/StudentsModel.php
@@ -32,24 +32,4 @@ class StudentsModel extends Model {
     {
         return $this->db->table('students')->where('id', $id)->delete();
     }
-
-    // Get paginated students with search
-    public function getStudents($search = '', $limit = 10, $offset = 0)
-    {
-        $query = $this->db->table('students');
-        if (!empty($search)) {
-            $query->where("lastname LIKE '%$search%' OR firstname LIKE '%$search%' OR email LIKE '%$search%'");
-        }
-        return $query->limit($limit, $offset)->get_all();
-    }
-
-    // Get total count of students with search
-    public function getTotal($search = '')
-    {
-        $query = $this->db->table('students');
-        if (!empty($search)) {
-            $query->where("lastname LIKE '%$search%' OR firstname LIKE '%$search%' OR email LIKE '%$search%'");
-        }
-        return $query->count_all_results();
-    }
 }
